5381bb2e3c013694a3f44f70c2f22843b7c7e4bd
Ainamaani Isaac <aina.isaac2002@gmail.com>
2022-10-05 19:46:51 +0530

    updated voice.dart

--- lib/sreens/voice.dart
+++ lib/sreens/voice.dart
@@ -44,7 +44,7 @@ class _VoicePageState extends State<VoicePage> {
                   flutterTts.setPitch(1);
                   flutterTts.setSpeechRate(0.5);
                   flutterTts.speak(
-                      "Kikoni hostels.In Kikoni,there are quite many and nice hostels.Hostel one,Olympia hostel.This is a mixed hostel located in Makerere Kikoni with both single and double rooms.All their rooms are self contained.A single room per semester costs two point two million shillings and a double room goes up to one point one million shillings.This hostel offered really high quality services and among these include,a big parking,armed guards for security,a hostel shuttle,a saloon,a swimming pool and paid Wifi.If interested,the custodian's contact is 0700861253");
+                      "Kikoni hostels.In Kikoni,there are quite many and nice hostels.Hostel one,Olympia hostel.This is a mixed hostel located in Makerere Kikoni with both single and double rooms.All their rooms are self contained.A single room per semester costs two point two million shillings and a double room goes up to one point one million shillings.This hostel offers really high quality services and among these include,a big parking,armed guards for security,a hostel shuttle,a saloon,a swimming pool and paid Wifi.If interested,the custodian's contact is 0,7,0,0,8,6,1,2,5,3");
                 },
               ),
             ),
@@ -60,7 +60,7 @@ class _VoicePageState extends State<VoicePage> {
                     minimumSize: Size(150, 150), backgroundColor: Colors.white),
                 onPressed: () {
                   flutterTts.speak(
-                      "Hostels located in Wandegeya,Hostel one,Aryan Hostel.This is a mixed hostel located along Nkiizi Road.It has both self contained and afew rooms which are not self contained.A single room goes up to one point eight million shillings,a double room goes up to nine hundred fifty thousand shillings whereas a triple room goes up to six hundred fifty shillings.Services offered at the hostel include a large parking space,a hostel shuttle,a gym to mention but a few.The custodian's contact is 0772431733");
+                      "Hostels located in Wandegeya,Hostel one,Aryan Hostel.This is a mixed hostel located along Nkiizi Road.It has both self contained, and afew rooms which are not self contained.A single room goes up to one point eight million shillings,a double room goes up to nine hundred fifty thousand shillings, whereas a triple room goes up to six hundred fifty shillings.Services offered at the hostel include a large parking space,a hostel shuttle,a gym to mention but a few.The custodian's contact is 0,7,7,2,4,3,1,7,3,3");
                 },
               ),
             ),
